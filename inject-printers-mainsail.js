e9b05302707c0d55e7e44014c6e192d1596e941c
Cole Deck <cole@deck.sh>
2022-11-28 18:19:15 +0000

    Quote escaping

--- inject-printers-mainsail.js
+++ inject-printers-mainsail.js
@@ -1,1 +1,1 @@
-localStorage.setItem("printers", "[{"hostname":"10.69.70.92","port":"7126"},{"hostname":"10.69.70.92","port":7125}]");
+localStorage.setItem("printers", '[{"hostname":"10.69.70.92","port":"7126"},{"hostname":"10.69.70.92","port":7125}]');
