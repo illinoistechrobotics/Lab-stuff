12976872b149a4479a0bfcb64be66240652daf34
Cole Deck <cole@deck.sh>
2022-11-28 19:13:15 +0000

    Update inject-printers-mainsail.js

--- inject-printers-mainsail.js
+++ inject-printers-mainsail.js
@@ -1,1 +1,1 @@
-localStorage.setItem("printers", '[{"hostname":"https://monoprice.illinoistechrobotics.org/monopricei3","port":"443"},{"hostname":"https://monoprice.illinoistechrobotics.org/cr10mini","port":443}]');
+localStorage.setItem("printers", '[{"hostname":"monopricei3.api.illinoistechrobotics.org","port":"443"},{"hostname":"cr10mini.api.illinoistechrobotics.org","port":443}]');
